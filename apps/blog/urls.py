937528eda4a2b7998f7ebf819923b1e1ab310977
Kanysh <kanyshomirzak@gmai.com>
2026-03-10 21:43:34 +0500

    add rate limiting and API documentation for user registration and token management

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from apps.blog.stats_view import stats_view
 from apps.blog.views import PostViewSet
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="post")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("stats/", stats_view, name="stats"),
+]
